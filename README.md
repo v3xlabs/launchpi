596c396e5a3bc257f8b5acfb45a4fe80fc1d92e8
Luc <luc@lucemans.nl>
2025-01-01 14:41:51 +0100

    Update README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Launchpi
+
+Open-source multi-functional midi controller platform for the novation launchpads.
--- daemon/src/main.rs
+++ daemon/src/main.rs
@@ -28,22 +28,22 @@ async fn main() {
 
     let mut controllers: Vec<Arc<Box<dyn Alles>>> = Vec::new();
 
-    let controller: Arc<Box<dyn Alles>> = Arc::new(LaunchpadMiniMk1::guess().unwrap());
-    controllers.push(controller.clone());
+    // let controller: Arc<Box<dyn Alles>> = Arc::new(LaunchpadMiniMk1::guess().unwrap());
+    // controllers.push(controller.clone());
     let controller2: Arc<Box<dyn Alles>> = Arc::new(LaunchpadMiniMk3::guess().unwrap());
     controllers.push(controller2.clone());
 
-    controller.initialize().unwrap();
+    // controller.initialize().unwrap();
     controller2.initialize().unwrap();
 
     // info!("Successfully started controller: {}", controller.name());
 
     // state.controllers.push(controller);
 
-    let mut script = scripts::soundboard::SoundboardScript::new();
+    // let mut script = scripts::soundboard::SoundboardScript::new();
 
-    let controller1 = controller.clone();
-    tokio::spawn(async move { controller1.run(&mut script).unwrap() });
+    // let controller1 = controller.clone();
+    // tokio::spawn(async move { controller1.run(&mut script).unwrap() });
 
     let mut script2 = scripts::soundboard::SoundboardScript::new();
 
@@ -57,7 +57,7 @@ async fn main() {
         },
     }
 
-    controller.clear().unwrap();
+    // controller.clear().unwrap();
     controller2.clear().unwrap();
 
     thread::sleep(Duration::from_millis(100));
