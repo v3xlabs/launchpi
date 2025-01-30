72a63cab9746ab8465f21d6246efb5d41b966eab
Luc <luc@lucemans.nl>
2025-01-30 22:44:43 +0100

    Update launchpi

--- daemon/src/api/mod.rs
+++ daemon/src/api/mod.rs
@@ -1,20 +1,19 @@
-use axum::{routing::get, Router};
+// use axum::{routing::get, Router};
 use std::sync::Arc;
-use tokio::net::TcpListener;
 
 use crate::state::AppState;
 
 mod routes;
 
 pub async fn serve(state: AppState) -> Result<(), axum::Error> {
-    let app = Router::new()
-        .route("/", get(root))
-        .route("/devices", get(routes::devices::get))
-        .with_state(Arc::new(state));
+    // let app = Router::new()
+    //     .route("/", get(root))
+    //     .route("/devices", get(routes::devices::get))
+    //     .with_state(Arc::new(state));
 
-    let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    // let listener = TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
-    axum::serve(listener, app).await.unwrap();
+    // axum::serve(listener, app).await.unwrap();
 
     Ok(())
 }
