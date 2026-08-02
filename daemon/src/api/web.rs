08e15e4e991dd41ae58fc04ac27342034e3dbc6c
Luc <luc@lucemans.nl>
2026-08-02 21:40:13 +0200

    Update web handling

--- daemon/src/api/web.rs
+++ daemon/src/api/web.rs
@@ -16,17 +16,21 @@ pub async fn serve(uri: Uri) -> Response {
         return StatusCode::NOT_FOUND.into_response();
     }
 
-    let Some(content) = Assets::get(asset).or_else(|| {
-        if asset.contains('.') {
-            None
-        } else {
-            Assets::get("index.html")
-        }
-    }) else {
+    let Some((served_asset, content)) =
+        Assets::get(asset)
+            .map(|content| (asset, content))
+            .or_else(|| {
+                if asset.contains('.') {
+                    None
+                } else {
+                    Assets::get("index.html").map(|content| ("index.html", content))
+                }
+            })
+    else {
         return StatusCode::NOT_FOUND.into_response();
     };
 
-    let content_type = mime_guess::from_path(asset)
+    let content_type = mime_guess::from_path(served_asset)
         .first_or_octet_stream()
         .essence_str()
         .parse::<HeaderValue>()
@@ -37,4 +41,19 @@ pub async fn serve(uri: Uri) -> Response {
         content.data.into_owned().to_vec(),
     )
         .into_response()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test]
+    async fn an_spa_route_serves_html() {
+        let response = serve("/devices".parse().unwrap()).await;
+
+        assert_eq!(
+            response.headers().get(header::CONTENT_TYPE).unwrap(),
+            "text/html"
+        );
+    }
 }
