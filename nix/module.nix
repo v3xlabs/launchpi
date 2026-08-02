d9cf52d40c62c501ba99308be24051296216d43c
Luc <luc@lucemans.nl>
2026-08-02 14:31:20 +0200

    Update module

--- nix/module.nix
+++ nix/module.nix
@@ -34,6 +34,26 @@ in {
       default = 3000;
       description = "Port on which Launchpi serves its web interface and API.";
     };
+
+    openFirewall = lib.mkEnableOption "opening the Launchpi port in the firewall";
+
+    configDir = lib.mkOption {
+      type = lib.types.path;
+      default = "/var/lib/launchpi/config";
+      description = "Directory containing Launchpi configuration files.";
+    };
+
+    stateDir = lib.mkOption {
+      type = lib.types.path;
+      default = "/var/lib/launchpi/state";
+      description = "Directory containing Launchpi runtime state.";
+    };
+
+    cacheDir = lib.mkOption {
+      type = lib.types.path;
+      default = "/var/cache/launchpi";
+      description = "Directory containing Launchpi cached assets.";
+    };
   };
 
   config = lib.mkIf cfg.enable {
@@ -48,19 +68,26 @@ in {
       };
     };
 
+    networking.firewall.allowedTCPPorts = lib.mkIf cfg.openFirewall [cfg.port];
+
     systemd.services.launchpi = {
       description = "Launchpi MIDI controller service";
       wantedBy = ["multi-user.target"];
       serviceConfig = {
+        CacheDirectory = "launchpi";
         ExecStart = "${cfg.package}/bin/launchpi";
         User = cfg.user;
         Group = cfg.group;
         Restart = "on-failure";
         RestartSec = 5;
+        StateDirectory = "launchpi";
       };
       environment = {
+        LAUNCHPI_CACHE_DIR = cfg.cacheDir;
+        LAUNCHPI_CONFIG_DIR = cfg.configDir;
         LAUNCHPI_HOST = cfg.host;
         LAUNCHPI_PORT = toString cfg.port;
+        LAUNCHPI_STATE_DIR = cfg.stateDir;
       };
     };
   };
